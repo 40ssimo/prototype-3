38114013b7be013e82b6b39fc448441d07bd9d3b
Muhammad Ichsan Pratama <ichsansanpratama@gmail.com>
2024-05-02 06:19:41 +0700

    commit new features

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource audioSource;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerAnim.SetTrigger("Jump_trig");
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            dirtParticle.Stop();
+            audioSource.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            gameOver = true;
+            dirtParticle.Stop();
+            audioSource.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+}
